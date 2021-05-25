c688146ee2f5421b66c7485b09b0f9a379a0db54
Francesco De Carlo <decarlof@gmail.com>
2021-05-25 18:47:23 -0500

    added author list

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='slack2bm',
     version=open('VERSION').read().strip(),
     #version=__version__,
-    author='Francesco De Carlo',
+    author='Francesco De Carlo, Tao Zhou, Fanny Rodolakis, Byeongdu Lee',
     author_email='decarlof@gmail.com',
     url='https://github.com/xray-imaging/slack2bm',
     packages=find_packages(),
